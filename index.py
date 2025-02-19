5b68ffe24f9f8766242f5588ef3d122c7759fd9f
Raghuram27r <ogiralaraghuram2002@gmail.com>
2025-02-19 10:54:45 +0530

    index.py

--- index.py
+++ index.py
@@ -301,3 +301,23 @@
 # list = list(map(int,input().split()))
 # list.reverse()
 # print(list)
+
+def sum_of_digits(arr):
+#     result = []
+#     for num in arr:
+#         digit_sum = 0
+#         for digit in str(num):
+#             digit_sum += int(digit)
+#         result.append(digit_sum)
+#     return result
+# input_list = [202, 89, 112, 88]
+# output_list = sum_of_digits(input_list)
+# print("Output:", output_list)
+
+# 2Q.
+
+# arr = [10, 30, 10, 20, 10, 20, 30, 10]
+# res=arr.count(10)
+# res1=arr.count(20)
+# res2=arr.count(30)
+# print("30 =>",res,"\n \n","20 =>",res1,"\n \n","30 =>",res2)
